8d7bedd15975545ee99acca09bd2b63cdbe6f1de
Aamos Riihinen <aamos.riihinen@tuni.fi>
2020-01-24 21:58:01 +0200

    Added error handling

--- fueleconomy.py
+++ fueleconomy.py
@@ -1,7 +1,21 @@
 # This program can be used to calculate various useful statistics about a car's
 # fuel economy.
 
-# TO DO: support for different units, error handling
+# TO DO: support for different units, rounding up to two decimals
+
+
+# check validity of user input
+def checkInput(input):
+
+    try:
+        if int(input) == 0:
+            print("\nError! 0 is not a valid input.")
+            return False
+        return True
+    except:
+        print("\nError!", input, "is not a valid input.")
+        return False
+
 
 # calculate volume of fuel used
 def calculateVolume():
@@ -9,13 +23,18 @@ def calculateVolume():
     print("\nPlease input your car's average fuel consumption"
     " (l/100km):")
     consumption = input(">")
+    if checkInput(consumption) == False:
+        return
 
     print("\nPlease input the distance traveled (km): ")
     distance = input(">")
+    if checkInput(distance) == False:
+        return
 
     volume = int(consumption) / 100 * int(distance)
     print("\nEstimated volume of fuel consumed: " + str(volume) + "l")
     return
+
 
 # calculate maximum range possible to drive
 def calculateRange():
@@ -23,24 +42,33 @@ def calculateRange():
     print("\nPlease input your car's average fuel consumption"
     " (l/100km):")
     consumption = input(">")
+    if checkInput(consumption) == False:
+        return
 
     print("\nPlease input the volume of fuel available (l)")
     volume = input(">")
+    if checkInput(volume) == False:
+        return
 
     distance = int(volume) / (int(consumption) / 100)
     print("\nEstimated maximum range: " + str(distance) + "km")
 
+
 # calculate average fuel consumption
-# TO DO
 def calculateConsumption():
     print("\nPlease input the volume of fuel consumed (l): ")
     volume = input(">")
+    if checkInput(volume) == False:
+        return
 
     print("\nPlease input the distance driven (km): ")
     distance = input(">")
+    if checkInput(distance) == False:
+        return
 
     consumption = int(volume) / int(distance) * 100
     print("\nEstimated fuel consumption: " + str(consumption) + "l/100km")
+
 
 def main():
 
